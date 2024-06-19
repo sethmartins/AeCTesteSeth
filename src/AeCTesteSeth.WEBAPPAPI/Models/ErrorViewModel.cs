93eb837e96026c720c3e4974377b8d476e57ab92
Seth Martins <seth.martins@gmail.com>
2024-06-19 14:40:00 -0300

    Excluindo mais lixo

--- src/AeCTesteSeth.WEBAPPAPI/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace AeCTesteSeth.WEBAPPAPI.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
