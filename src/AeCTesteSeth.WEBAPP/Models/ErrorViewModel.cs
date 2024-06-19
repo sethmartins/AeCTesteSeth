64f8aef805ec7d178287f1a287317e2bd12ff4ab
Seth Martins <seth.martins@gmail.com>
2024-06-19 07:36:45 -0300

    Retirando lixodo projeto

--- src/AeCTesteSeth.WEBAPP/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace AeCTesteSeth.WEBAPP.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
